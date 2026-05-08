78f8577b5a28ca886e09687814218fa6328b7a9a
Rosol29 <rosolmehdawi22@hotmail.com>
2026-05-08 17:47:19 -0400

    update

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000,speciaization :'Javascript' },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000,speciaization :'Paython' },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000,speciaization :'Java' },
     //... More employee records can be added here
   ];
 
@@ -35,3 +35,12 @@ function findEmployeeById(employeeId) {
      }
  }
 
+function findSpecialization (employeeSpec){
+    const specializationJs=employees.find(employee=>employee.speciaization === 'Javascript');
+    if(specializationJs ){
+        document.getElementById('employeesDetails').innerHTML=`<p> 1. ${specializationJs.name} - ${specializationJs.department} - ${specializationJs.salary} - ${specializationJs.speciaization} </p>`;
+        
+    }else{
+        document.getElementById('employeesDetails').innerHTML='no employee has javascript speciaization ';
+    }
+}
